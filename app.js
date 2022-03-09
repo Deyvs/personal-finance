b8604fd63bfe6de0923d526e7504b03efaf802cb
Deyvs <deyvisondasilva@gmail.com>
2022-03-09 20:56:49 -0300

    application review and env variables

--- app.js
+++ app.js
@@ -2,28 +2,12 @@ require('dotenv').config()
 const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');
-require('./db');
-const { balance } = require('./model');
+require('./src/repositories/db');
+const routes = require('./src/routes/finances');
 const PORT = process.env.PORT || 8001;
 
+// parse(transformar) o body no formato json
 server.use(bodyParser.json());
-
-server.get('/finances', async (req, res) => {
-    const finances = await balance.find({});
-    res.json(finances);
-});
-
-server.get('/finances/:financeId', async (req, res) => {
-    const financeId = req.params.financeId;    
-    const finance = await balance.findById(financeId);
-    res.json(finance);
-});
-
-server.post('/finances', async (req, res) => {
-    const body = req.body;
-
-    const created = await balance.create(body);
-    res.status(201).json(created); 
-})
+server.use(routes);
 
 server.listen(PORT, console.log(`Running on port ${PORT}`))
--- src/repositories/model.js
+++ src/repositories/model.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid')
 const { Schema } = mongoose;
 
 const balanceSchema = new Schema({
-    descripition:String,
+    _id: {
+        type: String,
+        default: uuidv4()
+    },
+    descripition: String,
     price: Number,
     category: String,
     paymentType: String,
-    operationType: String,
-    createdAt: Date
+    operationType: String,    
+}, {
+    timestamps: {} 
 });
 
 const balance = mongoose.model('balance', balanceSchema);
